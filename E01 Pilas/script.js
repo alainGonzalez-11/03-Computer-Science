0ec15bca9aa88a8c64fa888c5f091c0f6ba94bd3
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-05-10 08:45:29 -0600

    Updated E01 - T01 code to work with stacks

--- E01 Pilas/script.js
+++ E01 Pilas/script.js
@@ -3,13 +3,35 @@
 Entrada : mifuncion(['Manzana','Cebolla','Apio','Naranja','Papaya','Sandía','Melón'],4)
 Salida: ['Manzana','Cebolla','Apio','Naranja'].
 */
-document.getElementById('T01').addEventListener('click', task1);
+document.getElementById("T01").addEventListener("click", task1);
 
 function task1() {
-    alert(dividirPila(['Manzana','Cebolla','Apio','Naranja','Papaya','Sandía','Melón'], 4));
+  alert(
+    recortarPila(
+      ["Manzana", "Cebolla", "Apio", "Naranja", "Papaya", "Sandía", "Melón"],
+      4
+    )
+  );
 }
 
-function dividirPila(pila, numero){
-    let nuevaPila = pila.slice(0, numero);
-    return nuevaPila;
+function recortarPila(pila, numero) {
+  while (pila.length > numero) {
+    pila.pop();
+  }
+  return pila;
 }
+
+/*
+2.- Escribe una función “reemplazar” que tenga como parámetros una pila de elementos de tipo Number, y dos valores también de tipo Number “nuevo” y “viejo”, de forma que si el segundo valor aparece en algún lugar de la pila, sea reemplazado por el primero (Solo la primera vez), y hará pop de los elementos más nuevos.
+Entrada: reemplazar([3,2,3,4,6,8,1,2,5,5], 7, 2)
+Salida: [3,2,3,4,6,8,1,7]
+*/
+
+function obtenerElementos(pila, cantidad) {
+  
+}
+
+// Ejemplo de uso
+//let pila = ['Manzana','Cebolla','Apio','Naranja','Papaya','Sandía','Melón'];
+//let elementos = 4;
+//console.log(obtenerElementos(pila, elementos)); // Salida: ['Manzana','Cebolla','Apio','Naranja']
